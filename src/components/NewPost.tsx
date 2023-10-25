f0a70a09e8273f69e800634c7be505ae27bdd06c
villegust <ville.gust@gmail.com>
2023-10-25 21:01:34 +0200

    Image preview

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,75 +1,116 @@
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
 import Image from "next/image";
 
 const NewPost = () => {
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files && e.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        if (reader.result && typeof reader.result === "string") {
+          setImagePreview(reader.result);
+        }
+      };
+
+      reader.readAsDataURL(file);
+    } else {
+      setImagePreview(null);
+    }
+  };
+
   return (
     <section className="newpost-section">
       <div className="newpost-content">
         <div className="newpost-content__profileAndInput">
-          <Image
-            src="/images/sidebar/profile.png"
-            alt="Logo"
-            width={40}
-            height={35}
-          />
-          <textarea name="" id="" placeholder="What's happening?"></textarea>
+          <div className="newpost-content__profileAndInput__text">
+            <Image
+              src="/images/sidebar/profile.png"
+              alt="Logo"
+              width={40}
+              height={35}
+            />
+            <textarea name="" id="" placeholder="What's happening?"></textarea>
+          </div>
+          {imagePreview && <img src={imagePreview} alt="Image Preview" />}
         </div>
 
         <div className="newpost-content__insertAndPost">
           <div className="newpost-content__insertAndPost__insert">
-            <span>
-              <Image
-                src="/images/newPost/image.svg"
-                alt="Logo"
-                width={20}
-                height={20}
+            <label htmlFor="imageUpload">
+              <i>
+                <Image
+                  src="/images/newPost/image.svg"
+                  alt="Logo"
+                  width={20}
+                  height={20}
+                />
+              </i>
+              <input
+                type="file"
+                id="imageUpload"
+                accept="image/*"
+                onChange={handleImageChange}
               />
-            </span>
+            </label>
 
-            <span>
-              <Image
-                src="/images/newPost/gif.svg"
-                alt="Logo"
-                width={20}
-                height={20}
-              />
-            </span>
+            <label>
+              <i>
+                <Image
+                  src="/images/newPost/gif.svg"
+                  alt="Logo"
+                  width={20}
+                  height={20}
+                />
+              </i>
+            </label>
 
-            <span>
-              <Image
-                src="/images/newPost/vote.svg"
-                alt="Logo"
-                width={20}
-                height={20}
-              />
-            </span>
+            <label>
+              <i>
+                <Image
+                  src="/images/newPost/vote.svg"
+                  alt="Logo"
+                  width={20}
+                  height={20}
+                />
+              </i>
+            </label>
 
-            <span>
-              <Image
-                src="/images/newPost/smiley.svg"
-                alt="Logo"
-                width={20}
-                height={20}
-              />
-            </span>
+            <label>
+              <i>
+                <Image
+                  src="/images/newPost/smiley.svg"
+                  alt="Logo"
+                  width={20}
+                  height={20}
+                />
+              </i>
+            </label>
 
-            <span>
-              <Image
-                src="/images/newPost/plan.svg"
-                alt="Logo"
-                width={20}
-                height={20}
-              />
-            </span>
+            <label>
+              <i>
+                <Image
+                  src="/images/newPost/plan.svg"
+                  alt="Logo"
+                  width={20}
+                  height={20}
+                />
+              </i>
+            </label>
 
-            <span>
-              <Image
-                src="/images/newPost/pin.svg"
-                alt="Logo"
-                width={20}
-                height={20}
-              />
-            </span>
+            <label>
+              <i>
+                <Image
+                  src="/images/newPost/pin.svg"
+                  alt="Logo"
+                  width={20}
+                  height={20}
+                />
+              </i>
+            </label>
           </div>
           <div className="newpost-content__insertAndPost__post">
             <button>Make a post</button>
