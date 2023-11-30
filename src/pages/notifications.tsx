1a879023a73ec0b72cc95dc03496c357c6dfd88e
villegust <ville.gust@gmail.com>
2023-11-30 12:43:13 +0100

    Noticication not working, see if this fixes it

--- src/pages/notifications.tsx
+++ src/pages/notifications.tsx
@@ -1,29 +1,29 @@
 import React from "react";
-import { NextPageContext } from "next";
-import { getSession } from "next-auth/react";
+// import { NextPageContext } from "next";
+// import { getSession } from "next-auth/react";
 import Head from "next/head";
 
 import Header from "@/components/Header";
 import NotificationsFeed from "@/components/NotificationsFeed";
 
-export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context);
+// export async function getServerSideProps(context: NextPageContext) {
+//   const session = await getSession(context);
 
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: "/",
+//         permanent: false,
+//       },
+//     };
+//   }
 
-  return {
-    props: {
-      session,
-    },
-  };
-}
+//   return {
+//     props: {
+//       session,
+//     },
+//   };
+// }
 
 export default function Notifications() {
   return (
