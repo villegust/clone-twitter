40c7b09a22aa37a2123ead1af0619f4f5cc4b662
villegust <ville.gust@gmail.com>
2023-11-30 12:19:45 +0100

    Samll changes

--- src/oldStuff/MessageInbox.tsx
+++ src/oldStuff/MessageInbox.tsx
@@ -4,8 +4,6 @@
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
-// import MessageFeed from "@/oldStuff/messagecomps/MessageFeed";
-
 // const MessageInbox = ({ messageUser }: any) => {
 //   return (
 //     <>
